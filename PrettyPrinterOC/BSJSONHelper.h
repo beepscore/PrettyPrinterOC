7a78e254d63d19d20bdb051bfd26fc8ad428ddfd
beepscore <support@beepscore.com>
2017-05-05 16:31:32 -0700

    Add BSJSONHelper

---
+++ PrettyPrinterOC/BSJSONHelper.h
@@ -0,0 +1,13 @@
+//
+//  BSJSONHelper.h
+//  PrettyPrinterOC
+//
+//  Created by Steve Baker on 5/5/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSJSONHelper : NSObject
+
+@end
---
+++ PrettyPrinterOC/BSJSONHelper.m
@@ -0,0 +1,13 @@
+//
+//  BSJSONHelper.m
+//  PrettyPrinterOC
+//
+//  Created by Steve Baker on 5/5/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import "BSJSONHelper.h"
+
+@implementation BSJSONHelper
+
+@end
