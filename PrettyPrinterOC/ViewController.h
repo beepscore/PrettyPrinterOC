189526b8cfa508d187f64a9c5d7f03412b22f9ee
beepscore <support@beepscore.com>
2017-05-05 15:50:55 -0700

    Initial Commit

---
+++ PrettyPrinterOC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrettyPrinterOC
+//
+//  Created by Steve Baker on 5/5/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PrettyPrinterOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PrettyPrinterOC
+//
+//  Created by Steve Baker on 5/5/17.
+//  Copyright © 2017 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
